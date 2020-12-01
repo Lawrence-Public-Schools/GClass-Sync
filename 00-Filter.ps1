cab7155b7f947e65803696551ac5291fa2f824f6
Alam Arias <Alam.Arias@lawrence.k12.ma.us>
2020-12-01 11:01:50 -0500

    Enroll students by 14 days

--- 00-Filter.ps1
+++ 00-Filter.ps1
@@ -247,7 +247,7 @@ Function main_fixup_enrollments
     {
         $enrollments_O_FP = Join-Path -Path $OutputFolder -ChildPath "enrollments.xml"
         Write-Host -Object "Fixup enrollments: Loading"
-        $BeginLayway = [TimeSpan]::FromDays(0)
+        $BeginLayway = [TimeSpan]::FromDays(14)
         Read-ORenrollments -FolderPath $InputFolder | Limit-OREnrollmentByDate -BeginLayway $BeginLayway | process_fixup_enrollment -FolderPath_I $InputFolder -FolderPath_O $OutputFolder | Export-Clixml -Path $enrollments_O_FP
         Write-Host -Object "Fixup enrollments: Done"
     }
