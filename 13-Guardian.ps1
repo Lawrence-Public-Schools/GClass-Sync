3fba16eac92de99881be3b12a0a1e4c7bfb7eccb
Alam Arias <Alam.Arias@lawrence.k12.ma.us>
2022-07-02 13:18:40 -0400

    Update 13-Guardian.ps1
    Trim emails of whitespace

--- 13-Guardian.ps1
+++ 13-Guardian.ps1
@@ -52,37 +52,44 @@ Function Invite_guardian()
             $GuardianId = $_
             $Guardian = $null
             $StudentGuardian = $null
+            $Guardianemail = $null
             If ($GUsersHT.ContainsKey($GuardianId))
             {
                 $Guardian = $GUsersHT[$GuardianId]
-                $StudentGuardian = $StudentGuardians | Where-Object -Property InvitedEmailAddress -EQ -Value $Guardian.email
-                $StudentGuardianInvitation = $StudentGuardianInvitations | Where-Object -Property InvitedEmailAddress -EQ -Value $Guardian.email
+                $Guardianemail = $Guardian.email.Trim()
+                If ($Guardianemail -cne $Guardian.email)
+                {
+                    Write-Verbose -Message ("Email have whitespace in email: {0}" -f $GuardianId)
+                }
+                $StudentGuardian = $StudentGuardians | Where-Object -Property InvitedEmailAddress -EQ -Value $Guardianemail
+                $StudentGuardianInvitation = $StudentGuardianInvitations | Where-Object -Property InvitedEmailAddress -EQ -Value $Guardianemail
             }
+
             If ($Guardian -ceq $null)
             {
                 #Write-Verbose -Message ("Skipping {0} for blank email" -f $GuardianId)
             }
             ElseIf ($StudentGuardian -cne $null)
             {
-                #Write-Host -Object ("{0} already have {1}" -f $Student.Iemail,$Guardian.email)
+                #Write-Host -Object ("{0} already have {1}" -f $Student.Iemail,$Guardianemail)
             }
             ElseIf ($StudentGuardianInvitation -cne $null)
             {
-                #Write-Host -Object ("{0} already invite {1}" -f $Student.Iemail,$Guardian.email)
+                #Write-Host -Object ("{0} already invite {1}" -f $Student.Iemail,$Guardianemail)
             }
-            ElseIf ([String]::IsNullOrWhiteSpace($Guardian.email))
+            ElseIf ([String]::IsNullOrWhiteSpace($Guardianemail))
             {
                 Write-Verbose -Message ("Skipping {0} for empty email" -f $GuardianId)
             }
-            ElseIf ($Guardian.email -in $InvSented)
+            ElseIf ($Guardianemail -in $InvSented)
             {
-                Write-Verbose -Message ("Already sented {0} an invite" -f $Guardian.email)
+                Write-Verbose -Message ("Already sented {0} an invite" -f $Guardianemail)
             }
             Else
             {
-                $InvSented += $Guardian.email
-                New-GSStudentGuardianInvitation -StudentId $Student.email -GuardianEmail $Guardian.email
-                Write-Host -Object ("{0} invited {1}" -f $Student.email,$Guardian.email)
+                $InvSented += $Guardianemail
+                New-GSStudentGuardianInvitation -StudentId $Student.email -GuardianEmail $Guardianemail
+                Write-Host -Object ("{0} invited {1}" -f $Student.email,$Guardianemail)
             }
         } -End {
         } -Verbose
